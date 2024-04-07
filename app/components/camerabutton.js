0dc32c6de569755c13063c5002eddb6cd5ecdf2a
LucaSain <lucasainenco2@gmail.com>
2024-04-07 10:34:27 +0300

    das

--- app/components/camerabutton.js
+++ app/components/camerabutton.js
@@ -66,21 +66,19 @@ export default function CameraButton() {
               <path d="M149.1 64.8L138.7 96H64C28.7 96 0 124.7 0 160V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V160c0-35.3-28.7-64-64-64H373.3L362.9 64.8C356.4 45.2 338.1 32 317.4 32H194.6c-20.7 0-39 13.2-45.5 32.8zM256 192a96 96 0 1 1 0 192 96 96 0 1 1 0-192z" />
             </svg>
           ) : scanning == 1 ? (
-            <form
-              action={triggerBicicleta}
-              ref={formRef}
-              className="w-[90%] rounded-lg"
-            >
+            <div className="w-[90%] rounded-lg">
               <Scanner
-                onClick={() => {
-                  formRef.current.submit();
-                }}
                 components={{ audio: false, tracker: true }}
                 onResult={(result, text) => {
                   setData(text);
-                  if (formRef.current) {
-                    formRef.current.submit();
-                  }
+                  fetch("http://159.65.113.198:3001" + "/api/users/myInfo", {
+                    headers: {
+                      Authorization:
+                        "Bearer " +
+                        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjUsInVzZXJuYW1lIjoiQW5kcmVpIElvYW4iLCJpYXQiOjE3MTI0NjQzMjQsImV4cCI6MTcxMjU1MDcyNH0.XoVahPf6e8Wgv9T0LvfTlSDFtG9jpHJGvguYc28l-GY",
+                    },
+                    method: "POST",
+                  });
                   console.log(result, text);
                 }}
                 onError={(error) => {
@@ -89,7 +87,7 @@ export default function CameraButton() {
                 enabled={scanning == 1}
                 constraints={{ facingMode: { exact: "environment" } }}
               />
-            </form>
+            </div>
           ) : scanning == 2 ? (
             <div className="h-full w-full p-4 text-white">
               <div className="text-5xl font-bold">Ride started</div>
